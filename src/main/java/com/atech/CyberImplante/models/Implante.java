569ad2791c57662c6751c636842b0723aa12fcea
Anthony-gabriell <gabrielanthony540@gmail.com>
2026-08-31 20:48:27 -0300

    feat: criação dos models Usuario e Implante

--- src/main/java/com/atech/CyberImplante/models/Implante.java
+++ src/main/java/com/atech/CyberImplante/models/Implante.java
@@ -1,4 +1,60 @@
 package com.atech.CyberImplante.models;
 
 public class Implante {
+    private int idImplante;
+    private String nome;
+    private String categoria;
+    private String parteDoCorpo;
+    private int risco;
+
+    public Implante (int idImplante, String nome, String categoria, String parteDoCorpo, int risco) {
+        this.idImplante = idImplante;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.parteDoCorpo = parteDoCorpo;
+        this.risco = risco;
+    }
+    // JPA/spring para o banco de dados exige que  classe tenha um construtor vazio
+    public Implante(){}
+
+    // getters coletores setters seletores
+    public int getIdImplante() {
+        return idImplante;
+    }
+
+    public void setIdImplante(int idImplante) {
+        this.idImplante = idImplante;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getParteDoCorpo() {
+        return parteDoCorpo;
+    }
+
+    public void setParteDoCorpo(String parteDoCorpo) {
+        this.parteDoCorpo = parteDoCorpo;
+    }
+
+    public int getRisco() {
+        return risco;
+    }
+
+    public void setRisco(int risco) {
+        this.risco = risco;
+    }
 }
--- src/main/java/com/atech/CyberImplante/models/Usuario.java
+++ src/main/java/com/atech/CyberImplante/models/Usuario.java
@@ -1,4 +1,41 @@
 package com.atech.CyberImplante.models;
 
 public class Usuario {
+    private int idUsuario;
+    private String nome;
+    private int nivel;
+
+    public Usuario (int idUsuario, String nome, int nivel) {
+        this.idUsuario = idUsuario;
+        this.nome = nome;
+        this.nivel = nivel;
+    }
+
+    // JPA/spring para o banco de dados exige que  classe tenha um construtor vazio
+    public Usuario(){}
+
+    // getters coletores
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+    // setters seletores
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
 }
