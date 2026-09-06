379b5ab9a4d11213408ace2684a2e508c25b29b0
Anthony-gabriell <gabrielanthony540@gmail.com>
2026-09-06 17:07:53 -0300

    feat: UsuarioService concluido

--- src/main/java/com/atech/CyberImplante/service/UsuarioService.java
+++ src/main/java/com/atech/CyberImplante/service/UsuarioService.java
@@ -1,6 +1,34 @@
 package com.atech.CyberImplante.service;
 
+import com.atech.CyberImplante.models.Usuario;
+import com.atech.CyberImplante.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+
+    // final para garantir que a depedencia nao mude apos ser injetada
+    private final UsuarioRepository usuarioRepository;
+
+    // injeção de dependencia via construtor (alterantiva @Autowired)
+    public UsuarioService (UsuarioRepository usuarioRepository){
+    this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario adicionarUsuario( Usuario usuario){
+        boolean valnome = usuario.validarNome(usuario.getNome()); // delega a validação ao próprio usuario
+        if (!valnome){
+            throw new RuntimeException("Nome invalido!");
+        }
+        return usuarioRepository.save(usuario);
+    }
+
+
+
+
+
+
 
 
 }
+
