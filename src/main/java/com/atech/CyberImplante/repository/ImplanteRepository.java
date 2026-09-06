b6ba8fdfe1b57216101e76265bcb0a8129a2ab8c
Anthony-gabriell <gabrielanthony540@gmail.com>
2026-09-05 21:20:33 -0300

    feat: repository finalizado

--- src/main/java/com/atech/CyberImplante/repository/ImplanteRepository.java
+++ src/main/java/com/atech/CyberImplante/repository/ImplanteRepository.java
@@ -1,4 +1,14 @@
 package com.atech.CyberImplante.repository;
 
-public interface ImplanteRepository {
+// Abaixo eu tengo os Imports as anotações do banco de dados
+import com.atech.CyberImplante.models.Implante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Indica que esta interface é responsável pelo acesso aos dados
+public interface ImplanteRepository extends JpaRepository<Implante, Long> {
 }
+
+// JpaRepository<Implante, Long> significa:
+// "Vou gerenciar a entidade Implante e a chave primária (ID) dela é do tipo Long"
+// Não precisamos escrever metodos, ne referencias aqui, eles já nascem ao extender a interface.
--- src/main/java/com/atech/CyberImplante/repository/UsuarioRepository.java
+++ src/main/java/com/atech/CyberImplante/repository/UsuarioRepository.java
@@ -1,4 +1,15 @@
 package com.atech.CyberImplante.repository;
 
-public interface UsuarioRepository {
+// Abaixo eu tengo os Imports as anotações do banco de dados
+import com.atech.CyberImplante.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Indica que esta interface é responsável pelo acesso aos dados
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
+
+// JpaRepository<Implante, Long> significa:
+// "Vou gerenciar a entidade Usuario e a chave primária (ID) dela é do tipo Long"
+// Não precisamos escrever metodos, ne referencias aqui, eles já nascem ao extender a interface.
+
