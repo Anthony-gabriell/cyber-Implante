bbdb5ab9442832dbad5777a67e4b4f0ac728fbb1
Anthony-gabriell <gabrielanthony540@gmail.com>
2026-09-06 16:21:22 -0300

    feat: impkanteService implementação dos metodos

--- src/main/java/com/atech/CyberImplante/service/ImplanteService.java
+++ src/main/java/com/atech/CyberImplante/service/ImplanteService.java
@@ -1,4 +1,56 @@
 package com.atech.CyberImplante.service;
 
+import com.atech.CyberImplante.models.Implante;
+import com.atech.CyberImplante.models.Usuario;
+import com.atech.CyberImplante.repository.ImplanteRepository;
+import com.atech.CyberImplante.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service // registra esta classe como um componente de serviço do Spring
 public class ImplanteService {
+
+    // final para garantir que a depedencia nao mude apos ser injetada
+    private final ImplanteRepository implanteRepository;
+    private final UsuarioRepository usuarioRepository;
+
+    // injeção de dependencia via construtor (alterantiva @Autowired)
+    public ImplanteService(ImplanteRepository implanteRepository, UsuarioRepository usuarioRepository){
+        this.implanteRepository = implanteRepository;
+        this.usuarioRepository = usuarioRepository;
+    }
+    // metodo para instalar os implantes
+    public Implante instalarImplante(Long idImplante, Long idUsuario){
+
+        Implante implante = implanteRepository.findById(idImplante) // busca o implante no banco. retorna um optional um objeto que pode ou não ter valor.
+                .orElseThrow(() -> new RuntimeException("Implante não encontrado")); // se vazio lanca exceção em vez de retornar null
+
+        Usuario usuario = usuarioRepository.findById(idUsuario) // busca o usuaruo no banco retorna um optional um objeto que pode ou não ter valor.
+                .orElseThrow(() -> new IllegalArgumentException("Usuario não encontrado")); // se nao encontrou, lança exceção, se encontrou retorna o usuario
+
+      boolean temAcesso = usuario.validarNivel(implante.getNivel()); // delega a validação ao próprio usuario
+        if (!temAcesso){
+            throw new RuntimeException("Usuário não tem nível suficiente"); // persiste no banco e retorna o implante salvo com ID gerado
+        }
+
+        return implanteRepository.save(implante);
+
+    }
+
+    // metodo para listar implante
+    public List<Implante> listarImplante(){
+        // usamos findall() que vai no banco e busca tudo
+        List<Implante> todosImplantes = implanteRepository.findAll();
+
+        // for-each para cada tarefa dentro da lista
+        for (Implante implante : todosImplantes){
+            System.out.println(implante.getIdImplante() + " - " + implante.getNome() + " - " + implante.getCategoria() + " - " + implante.getParteDoCorpo());
+        }
+        return todosImplantes;
+
+    }
+
+
+
+
 }
